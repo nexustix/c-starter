638ea2451e2ec2819e0d3a6170283ac1fa01a7d0
nexustix <nexus1768@tuta.io>
2019-09-24 23:57:48 +0200

    Fix remove header wildcard

---
+++ src/main.h
@@ -0,0 +1,7 @@
+/* This file was automatically generated.  Do not edit! */
+#ifndef SRC_MAIN_H
+#define SRC_MAIN_H
+int WinMain(int argc,const char *argv[]);
+int main(int argc,const char *argv[]);
+#define INTERFACE 0
+#endif
